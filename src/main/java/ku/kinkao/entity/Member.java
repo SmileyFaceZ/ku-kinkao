40908e9661c0a0c6c3078a459fbf981bf006d180
Kantaphat PHAPHUI <bosskingblack10@gmail.com>
2024-11-28 19:14:54 +0700

    Add Random ID to Model classes

--- src/main/java/ku/kinkao/entity/Member.java
+++ src/main/java/ku/kinkao/entity/Member.java
@@ -5,6 +5,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import lombok.Data;
+import java.util.UUID;
 
 
 @Data
@@ -14,8 +15,7 @@ public class Member {
 
     @Id
     @GeneratedValue
-    private int id;
-
+    private UUID id;
 
     private String username;
     private String password;
