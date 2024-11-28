27c9e48a0b5c86ea3f3e75870442b074a0b820cf
Kantaphat PHAPHUI <bosskingblack10@gmail.com>
2024-11-28 18:01:50 +0700

    Add GitHub SSO

--- src/main/java/ku/kinkao/config/SecurityConfig.java
+++ src/main/java/ku/kinkao/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package ku.kinkao.config;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -8,10 +9,12 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserService;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import ku.kinkao.service.UserDetailsServiceImp;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 
 
 @Configuration
@@ -20,6 +23,13 @@ public class SecurityConfig {
 
     @Autowired
     private UserDetailsServiceImp userDetailsService;
+
+    @Autowired
+    private OidcUserService oidcUserService;
+
+    @Autowired
+    private ApplicationContext context;
+
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -43,6 +53,22 @@ public class SecurityConfig {
                         .deleteCookies("JSESSIONID", "remember-me")
                         .permitAll()
                 );
+
+        ClientRegistrationRepository repository =
+                context.getBean(ClientRegistrationRepository.class);
+
+
+        if (repository != null) {
+            http
+                    .oauth2Login(oauth2Login -> oauth2Login
+                            .clientRegistrationRepository(repository)
+                            .userInfoEndpoint(userInfo -> userInfo
+                                    .oidcUserService(oidcUserService)
+                            )
+                            .loginPage("/login").permitAll()
+                    );
+        }
+
 
         return http.build();
     }
